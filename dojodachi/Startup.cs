286cc8b630a3eed21398aacfb6f8513181ba1a1d
Ahrav Dutta <ahravdutta02@gmail.com>
2017-09-21 03:49:35 -0700

    dojodachi

--- dojodachi/Startup.cs
+++ dojodachi/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace calling_card
+namespace dojodachi
 {
     public class Startup
     {
@@ -16,17 +16,14 @@ namespace calling_card
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSession();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseSession();
             app.UseMvc();
         }
     }
